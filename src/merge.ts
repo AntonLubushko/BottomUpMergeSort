b7842c32a3892029060b8b9313babe1f5398a72f
Anton Liubushko <antonlubushko@gmail.com>
2025-01-18 10:08:15 +0200

    Complete algorithm fon Neiman. Version 1.

--- src/merge.ts
+++ src/merge.ts
@@ -34,15 +34,45 @@ function sort(arr: Array<number>): Array<number> {
     if (ordinaryArraysAmount % 2 === 0 && tailArrayLength === 0) {
       arr = sortEvenWithoutTail(arr, ordinaryArraysAmount, ordinaryArrayLength);
       ordinaryArraysAmount = Math.floor(ordinaryArraysAmount / 2);
+      tailArrayLength = 0;
       ordinaryArrayLength *= 2;
-      tailArrayLength = arr.length - ordinaryArraysAmount * ordinaryArrayLength;
       continue;
     }
 
     // case 2: odd and without a tail
     if (ordinaryArraysAmount % 2 === 1 && tailArrayLength === 0) {
-      arr = sortOddWithoutTail(arr, ordinaryArrayLength, tailArrayLength);
-      console.log("inside case 2", tailArrayLength);
+      arr = sortOddWithoutTail(arr, ordinaryArraysAmount, ordinaryArrayLength);
+      ordinaryArraysAmount = Math.floor(ordinaryArraysAmount / 2);
+      tailArrayLength = ordinaryArrayLength;
+      ordinaryArrayLength *= 2;
+      continue;
+    }
+
+    // case 3: even and with a tail
+    if (ordinaryArraysAmount % 2 === 0 && tailArrayLength !== 0) {
+      arr = sortEvenWithTail(
+        arr,
+        ordinaryArraysAmount,
+        ordinaryArrayLength,
+        tailArrayLength
+      );
+      ordinaryArraysAmount = Math.floor(ordinaryArraysAmount / 2);
+      ordinaryArrayLength *= 2;
+      continue;
+    }
+
+    // case 4: odd and with a tail
+    if (ordinaryArraysAmount % 2 === 1 && tailArrayLength !== 0) {
+      arr = sortOddWithTail(
+        arr,
+        ordinaryArraysAmount,
+        ordinaryArrayLength,
+        tailArrayLength
+      );
+      ordinaryArraysAmount = Math.floor(ordinaryArraysAmount / 2);
+      tailArrayLength = tailArrayLength + ordinaryArrayLength;
+      ordinaryArrayLength *= 2;
+      continue;
     }
   }
 
@@ -75,18 +105,79 @@ function sortEvenWithoutTail(
 function sortOddWithoutTail(
   arr: number[],
   ordinaryArraysAmount: number,
-  tailArrayLength: number
+  ordinaryArrayLength: number
 ): number[] {
-  return arr;
+  let resultArr: Array<number> = [];
+  for (
+    let indexPoint = 0;
+    indexPoint < (ordinaryArraysAmount - 1) * ordinaryArrayLength;
+    indexPoint += 2 * ordinaryArrayLength
+  ) {
+    let arrFirst = arr.slice(indexPoint, indexPoint + ordinaryArrayLength);
+    let arrSecond = arr.slice(
+      indexPoint + ordinaryArrayLength,
+      indexPoint + 2 * ordinaryArrayLength
+    );
+    let mergedArray = mergeSorted(arrFirst, arrSecond);
+    resultArr = resultArr.concat(mergedArray);
+  }
+
+  resultArr = resultArr.concat(arr.slice(-ordinaryArrayLength));
+
+  return resultArr;
 }
 
-let arr: number[] = Array.from({ length: 8 }, () =>
+function sortEvenWithTail(
+  arr: number[],
+  ordinaryArraysAmount: number,
+  ordinaryArrayLength: number,
+  tailArrayLength: number
+): number[] {
+  let resultArr: Array<number> = sortEvenWithoutTail(
+    arr,
+    ordinaryArraysAmount,
+    ordinaryArrayLength
+  );
+
+  resultArr = resultArr.concat(arr.slice(-tailArrayLength));
+
+  return resultArr;
+}
+
+function sortOddWithTail(
+  arr: number[],
+  ordinaryArraysAmount: number,
+  ordinaryArrayLength: number,
+  tailArrayLength: number
+): number[] {
+  let resultArr: Array<number> = sortEvenWithoutTail(
+    arr,
+    ordinaryArraysAmount - 1,
+    ordinaryArrayLength
+  );
+
+  let lastPair = mergeSorted(
+    arr.slice(-tailArrayLength - ordinaryArrayLength, -tailArrayLength),
+    arr.slice(-tailArrayLength)
+  );
+  resultArr = resultArr.concat(lastPair);
+  return resultArr;
+}
+
+let arr: number[] = Array.from({ length: 100000 }, () =>
   Math.floor(Math.random() * 100)
 ); // массив из случайных чисел
 
-console.log("Initial arrray ", arr);
-let res = sort(arr);
-console.log(res);
+console.warn(`Array length ${arr.length}`);
+let start = performance.now();
+let res = arr.sort();
+let end = performance.now();
+console.log(`Prototype sort Execution time: ${(end - start).toFixed(3)} ms`);
+
+start = performance.now();
+res = sort(arr);
+end = performance.now();
+console.log(`Fon Neiman sort Execution time: ${(end - start).toFixed(3)} ms`);
 
 /**
 let a1: number[] = Array.from({ length: Math.floor(Math.random() * 20) }, () =>
